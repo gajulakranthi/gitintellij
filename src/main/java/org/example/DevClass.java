b43c784bfd28ab8d392d34e5cc1d575f8c0f17c1
Kranthi <gajula.kranthikumar@gmail.com>
2022-10-11 21:29:02 +0800

    Second Commit

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -3,5 +3,6 @@ package org.example;
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("Devclass");
+        System.out.println("Added First Line");
     }
 }
