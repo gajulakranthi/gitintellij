4646577aa765db24ef92343ca694f2a0d5a04be7
gajulakranthi444 <gajulakranthikumargoud@gmail.com>
2022-10-11 23:41:24 +0800

    Update DevClass.java (#2)
    added3

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -4,6 +4,6 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("Devclass");
         System.out.println("Added First Line");
-        System.out.println("Added second Line from gajulakranthi444 branch added few2");
+        System.out.println("Added second Line from gajulakranthi444 branch added few3");
     }
 }
